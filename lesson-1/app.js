6e1c08d1086042fc2c62312e22a7b67bc8d70279
Mohamed Jalloh <codingwithmbj@gmail.com>
2026-03-27 00:21:47 -0500

    first three lessons

---
+++ lesson-1/app.js
@@ -0,0 +1,4 @@
+const message = "Hello There!";
+
+console.log(message);
+alert(message);
---
+++ lesson-2/app.js
@@ -0,0 +1,20 @@
+// 3 ways to assign variables
+
+// 1. let - can be changed later
+let fname = "John";
+
+console.log(fname);
+fname = "Johnny";
+console.log(fname);
+
+// 2. const - cannot be changed
+
+const lname = "Bravo";
+
+console.log(lname);
+
+// 3. var - lagacy mo longer used or recommended
+
+var mName = "James";
+
+console.log(mName);
